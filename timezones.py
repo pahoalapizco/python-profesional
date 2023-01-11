a044f1822bad4100ba9db50184529bd08327822c
paohoalapizco <paolac.alapizco@gmail.com>
2023-01-11 14:11:00 -0800

    :sparkles: Practicing with time zones

---
+++ timezones.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import pytz
+
+mexico_timezone = pytz.timezone("America/Mexico_City")
+mexico_date = datetime.now(mexico_timezone)
+str_time = mexico_date.strftime("%d/%m/%Y %H:%M:%S")
+print(f"Ciudad de México: {str_time}")
+
+culiacan_timezone = pytz.timezone("America/Mazatlan")
+culiacan_date = datetime.now(culiacan_timezone)
+str_time = culiacan_date.strftime("%d/%m/%Y %H:%M:%S")
+print(f"Culiacán: {str_time}")
+
+
+LA_ca_timezone = pytz.timezone("America/Los_Angeles")
+LA_ca_date = datetime.now(LA_ca_timezone)
+str_time = LA_ca_date.strftime("%m/%d/%Y %H:%M:%S")
+print(f"Los Angeles, CA. : {str_time}")
